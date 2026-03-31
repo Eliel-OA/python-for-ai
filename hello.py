1170b213a9683c48bd2f4238be22f06ed29a8fb7
Eliel-OA <elielatta33@gmail.com>
2026-03-31 09:22:57 +0200

    Refactor: replace Animal class with calculate_total function and add shopping cart example

--- hello.py
+++ hello.py
@@ -1,27 +1,9 @@
+import os
+def   calculate_total(items):
+    total=0
+    for item in items:
+        total+=item['price']*item['quantity']
+    return total
 
-class Animal:
-  def __init__(self, name):
-    self.name = name
-    
-  def eat(self):
-    return f"{self.name} is eating"
-  
-  def sleep(self):
-    return f"{self.name} is sleeping"
-  
-  
-  
-class Dog(Animal):
-  
-  def bark(self):
-    return f"{self.name} says woof"
-  
-
-my_dog = Dog("Max")
-
-my_dog2 = Dog("Jerem")
-
-my_dog.bark()
-my_dog.eat()
-my_dog.sleep()
-  
+shopping_cart=[{'name':'apple','price':0.5,'quantity':6},{'name':'banana','price':0.3,'quantity':8}]
+print(calculate_total(shopping_cart))
